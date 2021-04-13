acfceae95427fe820cea3301c785ab274f098a75
akamboj99 <63441813+akamboj99@users.noreply.github.com>
2021-04-13 11:58:15 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -2,8 +2,49 @@ import java.util.*;
 import java.io.*;
 
 public class Arrays {
-  
-  	public static void min_element(int arr[])
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int size;
+		System.out.println("Enter the size of array");
+		size=sc.nextInt();
+		int arr[]=new int[size];
+		System.out.println("Enter the elements of array");
+		for(int i=0;i<size;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		display(arr); //To print array elements
+		
+		reverse(arr); //To reverse the array elements
+		
+		min_element(arr); //To find minimum elements
+		
+		movezero(arr); //Move all zeros at the end
+	}
+	
+	public static void movezero(int arr[])
+	{
+		int n=arr.length;
+		int j=0;
+		for(int i=0;i<n;i++)
+		{
+			if(arr[i]!=0 && arr[j]==0)
+			{
+				int temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+			}
+			if(arr[j]!=0)
+			{
+				j++;
+			}
+		}
+		System.out.println("After moving zero to end, array looks like:");
+		display(arr);
+	}
+	
+	public static void min_element(int arr[])
 	{
 		int min=arr[0];
 		for(int i=1;i<arr.length;i++)
@@ -38,27 +79,5 @@ public class Arrays {
 			System.out.println(arr[i]+" ");
 		}
 	}
-  
-  
-	public static void main(String args[])
-	{
-		Scanner sc=new Scanner(System.in);
-		int size;
-		System.out.println("Enter the size of array");
-		size=sc.nextInt();
-		int arr[]=new int[size];
-		System.out.println("Enter the elements of array");
-		for(int i=0;i<size;i++)
-		{
-			arr[i]=sc.nextInt();
-		}
-		display(arr); //To print array elements
-		
-		reverse(arr); //To reverse the array elements
-		
-		min_element(arr); //To find minimum element
-	}
-	
-
 	
 }
