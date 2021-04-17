60b71b6c879262b4f688973f8307742225160080
akamboj99 <63441813+akamboj99@users.noreply.github.com>
2021-04-17 13:29:22 +0530

    Create BinaryTree.java

---
+++ BinaryTree.java
@@ -0,0 +1,16 @@
+public class BinaryTree {
+	public class TreeNode{
+		private int data;
+		private TreeNode left;
+		private TreeNode right;
+		
+		public TreeNode(int data) {
+			this.data=data;
+		}
+	}
+	public BinaryTree()
+	{
+		this.left=null;
+		this.right=null;
+	}
+}
