73cdc82e6929eed82e186e74fd3b794c494c9791
akamboj99 <63441813+akamboj99@users.noreply.github.com>
2021-04-24 16:48:42 +0530

    Create BinarySearchTree.java

---
+++ BinarySearchTree.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class BinarySearchTree {
+	
+	public class TreeNode{
+		private int data;
+		private TreeNode left;
+		private TreeNode right;
+		
+		public TreeNode(int data) {
+			this.data=data;
+		}
+	}
+}
