7be66b3b541a93688f96f656ab957ca3b57e2a4f
akamboj99 <63441813+akamboj99@users.noreply.github.com>
2021-04-25 12:59:44 +0530

    Create FInd_Element_in_Sorted_Matrix.java

---
+++ Other-Problems/FInd_Element_in_Sorted_Matrix.java
@@ -0,0 +1,37 @@
+
+public class SortedMatrix {
+	
+	public void search(int[][] matrix, int n, int key)
+	{
+		int i=0;
+		int j=n-1;
+		while(i<n && j>=0)
+		{
+			if(matrix[i][j] == key)
+			{
+				System.out.println(key + " found at " + i+","+j);
+				return;
+			}
+			if(matrix[i][j]>key)
+			{
+				j--;
+			}
+			else {
+				i++;
+			}
+		}
+		System.out.println("No Element found!! ");
+	}
+	
+	public static void main(String[] args)
+	{
+		int [][] matrix= {
+				           {10,20,30,40},
+				           {15,25,35,45},
+				           {27,29,37,48},
+				           {32,33,39,51}
+		                 };
+		SortedMatrix sm=new SortedMatrix();
+		sm.search(matrix, matrix.length,90);
+	}
+}
